09046d40611bc4e433f687c8d70965ff4578e087
lmmv9411 <luismmv9411@gmail.com>
2024-09-29 07:01:48 -0500

    ajustes juego

--- src/main/java/com/snake/SnakeGame.java
+++ src/main/java/com/snake/SnakeGame.java
@@ -3,7 +3,10 @@ package com.snake;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -12,7 +15,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -33,6 +35,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
 
     // Food
     private SnakePoint food;
+
     private Random random;
 
     // Game Logic
@@ -67,93 +70,12 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         gameLoop.start();
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        draw(g);
-    }
-
-    private void draw(Graphics g) {
-
-        // Food
-        g.setColor(Color.RED);
-        g.fill3DRect(food.x * cellSize, food.y * cellSize, cellSize, cellSize, true);
-
-        // Snake
-        // g.setColor(Color.GREEN);
-        Color startColor = Color.GREEN;
-        int snakeLength = snake.size();
-
-        for (int i = 0; i < snakeLength; i++) {
-
-            float factor = (snakeLength > 1) ? (float) i / (snakeLength - 1) : 0.0f;
-
-            int red = startColor.getRed();
-            int green = (int) (startColor.getGreen() * (1 - 0.3 * factor));
-            int blue = startColor.getBlue();
-
-            g.setColor(new Color(red, green, blue));
-
-            g.fill3DRect(snake.get(i).x * cellSize, snake.get(i).y * cellSize, cellSize, cellSize, true);
-        }
-
-        // for (final var point : snake) {
-        // g.fill3DRect(point.x * cellSize, point.y * cellSize, cellSize, cellSize,
-        // true);
-        // }
-
-        // Score
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.PLAIN, 16));
-
-        if (gameOver) {
-
-            gameLoop.stop();
-
-            var btnReanudar = new JButton("Reanudar");
-
-            btnReanudar.setBounds(25, 25, 100, 30);
-
-            btnReanudar.setVisible(true);
-
-            btnReanudar.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    resetGame();
-                    btnReanudar.setVisible(false);
-                    gameLoop.start();
-                }
-            });
-
-            this.add(btnReanudar);
-
-            g.setColor(Color.RED);
-            g.drawString("Game Over: " + String.valueOf(snake.size()), cellSize - 16, cellSize);
-
-        } else {
-            g.drawString("Score: " + String.valueOf(snake.size()), cellSize - 16, cellSize);
-        }
-    }
-
     private void resetGame() {
         gameOver = false;
         snake.clear();
         spawnFood();
         spawnHead();
         randomVelocity();
-    }
-
-    private void randomVelocity() {
-        int x = 0;
-        int y = 0;
-
-        while (y == 0 && x == 0) {
-            x = random.nextInt(3) - 1;
-            y = x == 0 ? random.nextInt(3) - 1 : 0;
-        }
-
-        velocityX = x;
-        velocityY = y;
     }
 
     private void spawnHead() {
@@ -185,6 +107,86 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         } while (onSnake);
 
         food = new SnakePoint(x, y);
+    }
+
+    private void randomVelocity() {
+        int x = 0;
+        int y = 0;
+
+        do {
+            x = random.nextInt(3) - 1;
+            y = x == 0 ? random.nextInt(3) - 1 : 0;
+        } while (y == 0 && x == 0);
+
+        velocityX = x;
+        velocityY = y;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    private void draw(Graphics g) {
+
+        // Food
+        g.setColor(Color.RED);
+        g.fill3DRect(food.x * cellSize, food.y * cellSize, cellSize, cellSize, true);
+
+        // Snake
+        Color startColor = Color.GREEN;
+        int snakeLength = snake.size();
+
+        for (int i = 0; i < snakeLength; i++) {
+
+            float factor = (snakeLength > 1) ? (float) i / (snakeLength - 1) : 0.0f;
+
+            int red = startColor.getRed();
+            int green = (int) (startColor.getGreen() * (1 - 0.3 * factor));
+            int blue = startColor.getBlue();
+
+            g.setColor(new Color(red, green, blue));
+
+            g.fill3DRect(snake.get(i).x * cellSize, snake.get(i).y * cellSize, cellSize, cellSize, true);
+        }
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        // Score
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+
+        if (gameOver) {
+            g.setColor(Color.RED);
+
+            gameLoop.stop();
+
+            FontMetrics fm = g.getFontMetrics();
+
+            String[] lines = {
+                    String.format("Game Over: %s", snake.size() - 1),
+                    "Press Space Key For Reset"
+            };
+
+            int centerX = boardWidth / 2;
+            int centerY = boardHeight / 2;
+
+            int lineHeight = fm.getHeight();
+
+            for (int i = 0; i < lines.length; i++) {
+                String line = lines[i];
+                int textWidth = fm.stringWidth(line);
+                int x = centerX - (textWidth / 2);
+                int y = centerY + (i * lineHeight);
+                g.drawString(line, x, y);
+            }
+
+        } else {
+            g.drawString("Score: " + (snake.size() - 1), cellSize / 2, cellSize);
+        }
     }
 
     private void move() {
@@ -254,6 +256,12 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
                 if (velocityX != 1) {
                     velocityX = -1;
                     velocityY = 0;
+                }
+                break;
+            case KeyEvent.VK_SPACE:
+                if (gameOver) {
+                    resetGame();
+                    gameLoop.start();
                 }
                 break;
             default:
